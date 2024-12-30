d00bac077d4eda776ea7872856416a82df36f4c9
mrranand <techanand9920@gmail.com>
2024-12-30 15:18:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ async def approve(_, m : Message):
     try:
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
-        await app.send_message(kk.id, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @AutoApprove_RequestBot __**".format(m.from_user.mention, m.chat.title))
+        await app.send_message(kk.id, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @mr_anand __**".format(m.from_user.mention, m.chat.title))
         add_user(kk.id)
     except errors.PeerIdInvalid as e:
         print("user isn't start bot(means group)")
@@ -55,12 +55,12 @@ async def op(_, m :Message):
         return 
     keyboard = InlineKeyboardMarkup(
         [[
-            InlineKeyboardButton("🗯 Channel", url="https://t.me/AutoApprove_RequestBot"),
             InlineKeyboardButton("💬 Support", url="https://t.me/Auto_RequestBot_discussion")
+            InlineKeyboardButton("🗯 Today's Deals", url="https://t.me/+EsL-cEvBSjQzOWU1"),
         ]]
     )
     add_user(m.from_user.id)
-    await m.reply_photo("https://graph.org/file/d57d6f83abb6b8d0efb02.jpg", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powered By : @VJ_Botz __**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
+    await m.reply_photo("", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powered By : @mr_anand __**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
     
 
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ callback ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -74,8 +74,8 @@ async def chk(_, cb : CallbackQuery):
         return 
     keyboard = InlineKeyboardMarkup(
         [[
-            InlineKeyboardButton("🗯 Channel", url="https://t.me/AutoApprove_RequestBot"),
-            InlineKeyboardButton("💬 Support", url="https://t.me/Auto_RequestBot_discussion")
+            InlineKeyboardButton("💬 Support", url="https://t.me/Auto_RequestBot_discussion"),
+            InlineKeyboardButton("🗯 Today's Deals", url="https://t.me/+EsL-cEvBSjQzOWU1")  
         ]]
     )
     add_user(m.from_user.id)
